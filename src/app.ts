33de4cac5197d34fa2e8a04cb8a360c859b295d5
Navraj Singh <navrajsingh@rrc.ca>
2025-12-03 08:29:17 -0600

    Main push

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import { calculatePortfolioPerformance, largestholdingasset, calculateAllocation } from "../src/portfolio/portfolioPerformance";
 const app: Express = express();
+//Scan trigger   
 
 interface HealthCheckResponse {
     status:string;
